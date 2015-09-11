b7cf5e4440402314bf1b10d4a8613772ba4b467a
Rudzainy Rahman <rudzainy@gmail.com>
2015-09-11 19:18:19 +0800

    Beautify header, implement user input & implement disable board features

--- public/js/application.js
+++ public/js/application.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
 
-  game_state = "01o02o";
+  game_state = "02x11o20x22o00x";
+  player_shape = "x"
 	drawTable();
 	populateTable(game_state);
+	getMove();
   
 });
 
 function drawTable(){
 	row = 0;
+	index = 0;
 	while(row < 3) {
 		if(row == 0){
 			position = "left";
@@ -22,7 +25,12 @@ function drawTable(){
 		$('#board-container').append('<div id="row-' + position + '">')
 		col = 0;
 		while(col < 3) {
-			$('#board-container').append('<div id="box[' + row + '][' + col + ']" class="box">x</div>')
+			if(index % 2 == 0){
+				$('#board-container').append('<button id="' + row + '' + col + '" class="box"></button>')
+			}else{
+				$('#board-container').append('<button id="' + row + '' + col + '" class="box" style="background-color: #00DAAB;"></button>')
+			}
+			index++;
 			col++;
 		}
 		$('#board-container').append('</div>')
@@ -32,20 +40,28 @@ function drawTable(){
 
 function populateTable(game_state){ // Import game state
 	moves = game_state.match(/(.{3})/g);
-	debugger
 	for(count = 0; count < moves.length; count++){
-		var box = document.getElementById('box[' + parseInt(moves[count][0]) + '][' + parseInt(moves[count][1]) + ']');
+		var box = document.getElementById('' + parseInt(moves[count][0]) + parseInt(moves[count][1]) + '');
 		box.innerHTML = moves[count][2];
 	};
 };
 
-String.prototype.scan = function (re) {
-    if (!re.global) throw "ducks";
-    var s = this;
-    var m, r = [];
-    while (m = re.exec(s)) {
-        m.shift();
-        r.push(m);
-    }
-    return r;
+function getMove(){
+	// alert("In!");
+	$(document).on('click', '.box', function(event){
+		debugger
+		box_id = $(this).attr('id');
+		game_state += box_id + player_shape;
+		var box = document.getElementById('' + parseInt(box_id[0]) + parseInt(box_id[1]) + '');
+		box.innerHTML = player_shape;
+		
+		var all_box = document.getElementsByClassName('box')
+		$.each(all_box, function(index, box){
+			box.disabled = true;
+		});
+	});
 };
+
+function winningCondition(){
+	
+}
