937a5e1c4f13b98da752c95246f77385c8c8b4a2
Rudzainy Rahman <rudzainy@gmail.com>
2015-12-12 18:54:52 +0800

    Rename migration files

---
+++ db/migrate/20150911102923_create_game_histories.rb
@@ -0,0 +1,9 @@
+class CreateGameHistories < ActiveRecord::Migration
+  def change
+  	create_table :game_histories do |t|
+  		t.integer :game_id
+  		t.string :history
+  		t.timestamps null: false
+  	end
+  end
+end
