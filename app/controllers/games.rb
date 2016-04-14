32e5a2ddd4a8e95efc55e72507bda687e7460db5
rudzainy <rudzainy@gmail.com>
2016-04-14 21:42:50 +0800

    Add awesome print

--- app/controllers/games.rb
+++ app/controllers/games.rb
@@ -1,12 +1,12 @@
-post '/create_game' do
+get '/create_game' do
 	session[:game_id] = Game.create(player1_id: current_user.id)
-	# redirect to '/lobby'
+	redirect to '/lobby'
 end
 
 get '/join_game/:game_id' do
 	game = Game.find(params[:game_id])
 	game.update(player2_id: current_user.id)
-	redirect to '/game/#{game.id}'
+	redirect to "/game/#{game.id}"
 end
 
 get '/game/:game_id' do
